b5830deba9fcde7be29a40a552d090adf21254cb
enaikele omoh kelvin <enaikeleomoh@gmail.com>
2023-03-30 14:33:29 +0100

    updated"

--- drugs_ai/src/Component/Navigation.js
+++ drugs_ai/src/Component/Navigation.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import {AiOutlineHome} from "react-icons/ai"
 import {FiLogOut} from "react-icons/fi"
+import {  useNavigate } from 'react-router-dom'
+// import { useHistory } from 'react-router-dom';
 import Logo from  '../Assets/logo.png' 
 const Navigation = () => {
+  const navigate=useNavigate()
+
+  const LogOut=()=>{
+    navigate("/")
+  }
   return (
     <div className=' flex flex-col gap-5 w-[30vh] bg-[white] p-4 h-[100vh]'>
           {/* NAVIGATION */}
@@ -21,7 +28,10 @@ const Navigation = () => {
     
         <div className=' flex gap-4 justify-center items-center  flex-col '> 
         <div className=' w-full bg-[#4750AB] flex justify-center items-center cursor-pointer gap-4 text-white p-4 rounded-md '><AiOutlineHome size={30}/> Dashboard </div>
-        <div className='w-full  active:bg-[#4750AB] text-[#898989] flex justify-center cursor-pointer items-center gap-3 p-4 rounded-md active:text-white'><FiLogOut className='mr-6' size={30}/> Signout </div>
+      
+      <div onClick={LogOut}  className='w-full  active:bg-[#4750AB] text-[#898989] flex justify-center cursor-pointer items-center gap-3 p-4 rounded-md active:text-white'><FiLogOut className='mr-6' size={30}/> 
+      
+         Signout </div>  
        
         </div>
         
